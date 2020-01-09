4297f199acd74215b30e28b1a433d997603c029c
이 종원 <jong12em@naver.com>
2020-01-09 16:53:41 +0900

    [기타] 안드로이드 프로젝트 생성 및 규칙 리드미 지정
    안드로이드 프로젝트 생성
    리드미에 공부용으로 깃 사용 규칙 정리

---
+++ app/src/main/java/jongwonleee/under_armour/MainActivity.kt
@@ -0,0 +1,12 @@
+package jongwonleee.under_armour
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
